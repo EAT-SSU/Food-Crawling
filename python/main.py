697962c1ba1d59824a01af1bb86f55825acb7e57
rover0811 <rover0811@hotmail.com>
2023-02-12 22:33:57 +0900

    [Add] Add description

--- python/main.py
+++ python/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
     sdt   -> 날짜
 
 '''
-@app.get("/foods/{day}/{restaurantType}",description="도담식당과 학생식당의 메뉴를 받아오는 api입니다. restaurantType={1:학생식당,2:숭실도담식당} day={yyyymmdd} 현재 도담식당만 가능합니다.")
+@app.get("/foods/{day}/{restaurantType}",description="도담식당과 학생식당의 메뉴를 받아오는 api입니다. restaurantType={1:학생식당,2:숭실도담식당} day={yyyymmdd} 도담식당이랑 학생식당만 가능합니다.")
 def getTodayMenu(restaurantType:int, date:str):
     webpage=urllib.request.urlopen(f'http://m.soongguri.com/m_req/m_menu.php?rcd={restaurantType}&sdt={date}')
     soup:BeautifulSoup = BeautifulSoup(webpage, 'html.parser')
@@ -28,7 +28,6 @@ def getTodayMenu(restaurantType:int, date:str):
         return(soup.find(text=re.compile("^뚝")).strip())
 
         #일단 학생식당 부분은 pass
-
     elif (restaurantType==2): #도담식당
         json={"중식":"","석식":""}
         result=soup.find_all("tr")
