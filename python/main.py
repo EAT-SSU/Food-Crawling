c4c5d068ab15f4e7a7c4e3afbefa1fe4672e2089
rover0811 <rover0811@hotmail.com>
2023-02-20 21:27:42 +0900

    [Docs] Add a docstring from chatgpt

--- python/main.py
+++ python/main.py
@@ -4,7 +4,7 @@ import re
 from typing import Optional
 from fastapi import FastAPI
 from datetime import datetime
-
+from typing import *
 
 app = FastAPI()
 
@@ -17,16 +17,35 @@ app = FastAPI()
 
 '''
 @app.get("/foods/{day}/{restaurantType}",description="도담식당과 학생식당의 메뉴를 받아오는 api입니다. restaurantType={1:학생식당,2:숭실도담식당} day={yyyymmdd} 도담식당이랑 학생식당만 가능합니다.")
-def getTodayMenu(restaurantType:int, date:str):
+def getTodayMenu(restaurantType:int, date:str) -> Union[str, Dict[str, List[str]], None]:
+    """
+    Fetches the menu for a given date and restaurant type.
+
+    Parameters:
+        - restaurantType (int): The type of the restaurant to query. 
+          Possible values are 1 (학생식당), 2 (숭실도담식당), or 3 (기숙사 식당).
+        - date (str): The date in the format 'yyyymmdd' for which to fetch the menu.
+
+    Returns:
+        - str: A message indicating that the restaurant is closed on weekends, if the requested date falls on a weekend.
+        - Dict[str, List[str]]: A dictionary containing the menu for both lunch and dinner (중식 and 석식) 
+          for the requested date and restaurant type, if the requested restaurant is '숭실도담식당'.
+        - None: If the requested restaurant type is not supported.
+
+    Note:
+        This function scrapes the mobile website of 숭실대학교(Soongsil University) cafeteria to fetch the menu.
+        Therefore, the function may fail if the website structure changes or if the website is down.
+    """
+        
     webpage=urllib.request.urlopen(f'http://m.soongguri.com/m_req/m_menu.php?rcd={restaurantType}&sdt={date}')
     soup:BeautifulSoup = BeautifulSoup(webpage, 'html.parser')
 
     inputDate = datetime.strptime(date,'%Y%m%d')
     if (inputDate.weekday()==5 or inputDate.weekday()==6):
         return "주말은 쉽니다"
-    elif (restaurantType==1): #학생식당
+    
+    if (restaurantType==1): #학생식당
         return(soup.find(text=re.compile("^뚝")).strip())
-
         #일단 학생식당 부분은 pass
     elif (restaurantType==2): #도담식당
         json={"중식":"","석식":""}
@@ -37,3 +56,5 @@ def getTodayMenu(restaurantType:int, date:str):
             elif(i.td.string=="석식1"):
                 json["석식"]=[ k.strip().lstrip("#") for k in i.find_all(text=re.compile("#.*"))]
         return json
+    elif (restaurantType==3): #기숙사 식당
+        pass
