037a442c85a9058b486cbf5f6f6be22002dfc5d9
rover0811 <rover0811@hotmail.com>
2023-04-13 16:23:51 +0900

    [Feat] 학생식당, 도담식당 return 모델

--- python/main.py
+++ python/main.py
@@ -2,9 +2,19 @@ import urllib.request
 from bs4 import BeautifulSoup
 import re
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI,status
 from datetime import datetime
 from typing import *
+from html_table_parser import parser_functions as parser
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+
+class SoongsilMenuModel(BaseModel):
+    date: Union[str,None]= None
+    restaurantType: Union[str,None] = None
+    lunch: Union[dict,list, None] = None
+    dinner: Union[list, None] = None
 
 app = FastAPI()
 
@@ -16,8 +26,10 @@ app = FastAPI()
     sdt   -> 날짜
 
 '''
-@app.get("/foods/{day}/{restaurantType}",description="도담식당과 학생식당의 메뉴를 받아오는 api입니다. restaurantType={1:학생식당,2:숭실도담식당} day={yyyymmdd} 도담식당이랑 학생식당만 가능합니다.")
-def getTodayMenu(restaurantType:int, date:str) -> Union[str, Dict[str, List[str]], None]:
+@app.get("/foods/{day}/{restaurantType}",
+        description="도담식당과 학생식당의 메뉴를 받아오는 api입니다. restaurantType={1:학생식당,2:숭실도담식당} day={yyyymmdd} 도담식당이랑 학생식당만 가능합니다.",
+        response_model=SoongsilMenuModel)
+def getTodayMenu(restaurantType:int, date:str) -> SoongsilMenuModel:
     """
     Fetches the menu for a given date and restaurant type.
 
@@ -37,38 +49,29 @@ def getTodayMenu(restaurantType:int, date:str) -> Union[str, Dict[str, List[str]
         Therefore, the function may fail if the website structure changes or if the website is down.
     """
         
-    webpage=urllib.request.urlopen(f'http://m.soongguri.com/m_req/m_menu.php?rcd={restaurantType}&sdt={date}')
-    soup:BeautifulSoup = BeautifulSoup(webpage, 'html.parser')
+    soongguri_webpage=urllib.request.urlopen(f'http://m.soongguri.com/m_req/m_menu.php?rcd={restaurantType}&sdt={date}')
+    soup:BeautifulSoup = BeautifulSoup(soongguri_webpage, 'html.parser')
 
     inputDate = datetime.strptime(date,'%Y%m%d')
     if (inputDate.weekday()==5 or inputDate.weekday()==6):
-        return "주말은 쉽니다"
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,content=SoongsilMenuModel(date=date).dict())
     
     if (restaurantType==1): #학생식당
-        json={}
-        json["중식1"]=soup.find(text=re.compile("^뚝")).strip()
-        json["식당"]="학생식당"
-        json["날짜"]=date
-        return(json)
-        #일단 학생식당 부분은 pass
+        school_cafeteria_lunch=[soup.find(text=re.compile("^뚝")).strip()]
+        return JSONResponse(status_code=status.HTTP_200_OK,content=SoongsilMenuModel(date=date,restaurantType="학생식당",lunch=school_cafeteria_lunch).dict())
     elif (restaurantType==2): #도담식당
-        json={}
-        json["식당"]="도담식당"
-        json["날짜"]=date
-        result=soup.find_all("tr")
-        for i in result:
+        tr_result=soup.find_all("tr")
+        dodam_restaurant_model=SoongsilMenuModel(lunch=dict(),restaurantType="도담식당",date=date)
+        for i in tr_result:
             if (i.td.string=="중식1"):
                 # json["중식"]=[ k.strip().lstrip("#") for k in i.find_all(text=re.compile("#.*"))] # 2022년도 규칙 #뒤에 메뉴
-                json["중식1"]=[k.next_element.text for k in i.find_all(text=re.compile("★.*"))] #2023년도 규칙 ★다음 요소에 메뉴
-                # json["중식"]=[k.text for k in i.find_all('font', {'color': '#ff9900'})] #2023년도 규칙 폰트 컬러가 #ff9900
+                dodam_restaurant_model.lunch["중식1"]=[k.next_element.text for k in i.find_all(text=re.compile("★.*"))] #2023년도 규칙 ★다음 요소에 메뉴
             elif (i.td.string=="중식4"):
-                json["중식4"]=[k.next_element.text for k in i.find_all(text=re.compile("★.*"))]
-
-
+                dodam_restaurant_model.lunch["중식4"]=[k.next_element.text for k in i.find_all(text=re.compile("★.*"))] #2023년도 규칙 ★다음 요소에 메뉴
             elif(i.td.string=="석식1"):
                 # json["석식"]=[ k.strip().lstrip("#") for k in i.find_all(text=re.compile("#.*"))]
-                json["석식1"]=[k.next_element.text for k in i.find_all(text=re.compile("★.*"))]
+                dodam_restaurant_model.dinner=[k.next_element.text for k in i.find_all(text=re.compile("★.*"))]
+        return JSONResponse(status_code=status.HTTP_200_OK,content=dodam_restaurant_model.dict())
 
-        return json
     elif (restaurantType==3): #기숙사 식당
         pass
