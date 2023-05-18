f70d81a86bb439a6edef6c6f5186e4b551f9452f
rover0811 <rover0811@hotmail.com>
2023-05-18 17:16:19 +0900

    [Add] crontab 기능을 위한 스크립트

---
+++ python/cron.py
@@ -0,0 +1,15 @@
+from crontab import CronTab
+import sys
+
+sys.path.append("/app/python/")
+
+# crontab 객체 생성
+cron = CronTab(user='rover0811')  # 사용자명을 설정하여 crontab을 사용합니다.
+
+# cron 작업 추가
+job = cron.new(command='python ./api-notification.py')  # 실행할 명령어를 지정합니다.
+job.setall('0 1 * * 1-6')  # 월요일부터 토요일까지 01:00에 실행하도록 스케줄을 설정합니다.
+
+
+# cron 작업 저장
+cron.write()
