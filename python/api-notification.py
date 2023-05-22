4746e2637aacf33c2f9adda1d9863e72f579d1c1
rover0811 <rover0811@hotmail.com>
2023-05-22 20:42:33 +0900

    [Fix] import 문 수정, main.py 잡다한 것 삭제

--- python/api-notification.py
+++ python/api-notification.py
@@ -1,12 +1,10 @@
 import requests
 import json
-from Object import Dodam,School_Cafeteria
+from Object import Dodam, School_Cafeteria
 import sentry_sdk
 from sentry_sdk.crons import monitor
-# from fastapi.encoders import jsonable_encoder
 from dotenv import load_dotenv
 import os
-
 
 
 from datetime import datetime
@@ -19,9 +17,6 @@ today = current_time.date().strftime("%Y%m%d")
 load_dotenv()
 
 
-
-
-
 sentry_sdk.init(
     dsn=os.environ.get("SENTRY_DSN"),
 
@@ -31,6 +26,7 @@ sentry_sdk.init(
     traces_sample_rate=1.0,
 )
 
+
 def send_slack_message(api_response):
     """
     Slack Incoming Webhooks를 사용하여 메시지를 보내는 함수
@@ -38,14 +34,16 @@ def send_slack_message(api_response):
     :param slack_webhook_url: Slack Incoming Webhooks URL
     :return: 성공 여부
     """
-    slack_webhook_url=os.environ.get("SLACK_URL")
+    slack_webhook_url = os.environ.get("SLACK_URL")
 
-    if api_response['restaurant_type']=='1':api_response['restaurant_type']="학생식당"
-    elif api_response['restaurant_type']=='2':api_response['restaurant_type']="도담식당"
+    if api_response['restaurant_type'] == '1':
+        api_response['restaurant_type'] = "학생식당"
+    elif api_response['restaurant_type'] == '2':
+        api_response['restaurant_type'] = "도담식당"
 
-    
-    text=f"날짜: {api_response['date']}\n식당: {api_response['restaurant_type']}\n메뉴: {api_response['menu']}"
-    body={"channel": "#api-notification","username": "학식봇","text": text,"icon_emoji": ":ghost:"}
+    text = f"날짜: {api_response['date']}\n식당: {api_response['restaurant_type']}\n메뉴: {api_response['menu']}"
+    body = {"channel": "#api-notification", "username": "학식봇",
+            "text": text, "icon_emoji": ":ghost:"}
     response = requests.post(slack_webhook_url, data=json.dumps(body))
     if response.status_code != 200:
         raise ValueError('Slack message sending failed')
@@ -54,19 +52,13 @@ def send_slack_message(api_response):
 
 @monitor(monitor_slug='qq')
 def testing_Dodam(date):
-    try:
-        api_response = Dodam(date=date).get_menu()
-        send_slack_message(api_response)
-    except Exception as e:
-        sentry_sdk.capture_exception(e)
+    api_response = Dodam(date=date).get_menu()
+    print(api_response, type(api_response))
+    send_slack_message(api_response)
+
 
 @monitor(monitor_slug='qq')
 def testing_School_Cafeteria(date):
-    # try:
-    #     api_response = School_Cafeteria(date=date)
-    #     send_slack_message(api_response)
-    # except Exception as e:
-    #     sentry_sdk.capture_exception(e)
     api_response = School_Cafeteria(date=date).get_menu()
     print(api_response, type(api_response))
     send_slack_message(api_response)
@@ -77,13 +69,10 @@ def testing_School_Cafeteria(date):
 # # testing_School_Cafeteria()
 
 if __name__=="__main__":
-    # testing_Dodam()
-    # print("Good!")
-    # testing_School_Cafeteria(date="20230525")
-
-
-    # for i in range(2,7):
-    #     testing_School_Cafeteria(date=f"2023052{i}")
+    '''
+        TODO: 이 부분이 CRON되는 곳.
+    '''
+    pass
 
 
 
