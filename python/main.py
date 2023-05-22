3e740e5fc1316644b430560b1836477179a90be2
rover0811 <rover0811@hotmail.com>
2023-05-22 20:42:33 +0900

    [Fix] import 문 수정, main.py 잡다한 것 삭제

--- python/main.py
+++ python/main.py
@@ -1,15 +1,14 @@
-from typing import Optional
-from fastapi import FastAPI,status
-from typing import *
-from Object import Dodam_or_School_Cafeteria ,Dodam
-from Object import Dormitory
-import os
-# from fastapi.responses import JSONResponse
-# from pydantic import BaseModel
-import sys
-sys.path.append("/app/python/")
-
 import sentry_sdk
+from fastapi import FastAPI
+
+from Object import School_Cafeteria
+from Object import Dormitory
+from Object import Dodam
+
+import os
+import sys
+
+sys.path.append("/app/python/")
 
 
 sentry_sdk.init(
@@ -24,48 +23,37 @@ sentry_sdk.init(
 
 app = FastAPI()
 
-'''
-    ?    rcd=2  &   sdt=20230206
-    rcd=1 -> 학생식당
-    rcd=2 -> 숭실도담식당
-
-    sdt   -> 날짜
-
-'''
-
 
 @app.get("/foods/dodam",
-        description="도담식당의 하루 치 메뉴를 받아오는 api입니다. date format->yyyymmdd. ex)20230502")
-def get_dodam(date:str):
-    # date = datetime.strptime(date,'%y%m%d')
-    todaymenu=Dodam_or_School_Cafeteria(2,date)   
-    return todaymenu.get_menu()
+         description="도담식당의 하루 치 메뉴를 받아오는 api입니다. date format->yyyymmdd. ex)20230502")
+def get_dodam(date: str):
+    today = Dodam(date=date)
+    today.get_menu()
+
+    return today.menu
+
 
 @app.get("/foods/school_cafeteria",
-        description="학생식당의 하루 치 메뉴를 받아오는 api입니다, date format->yyyymmdd. ex)20230502")
-def get_school_cafeteria(date:str):
-    todaymenu=Dodam_or_School_Cafeteria(1,date)   
-    a=todaymenu.get_menu()
-    return todaymenu.menu
+         description="학생식당의 하루 치 메뉴를 받아오는 api입니다, date format->yyyymmdd. ex)20230502")
+def get_school_cafeteria(date: str):
+    today = School_Cafeteria(date=date)
+    today.get_menu()
+
+    return today.menu
+
 
 @app.get("/foods/dormitory",
-        description="기숙사 식당의 일주일 치 메뉴를 받아오는 api입니다, date format->yyyymmdd. ex)20230502")
-def get_school_cafeteria(date:str):
-    todaymenu=Dormitory(date)   
+         description="기숙사 식당의 일주일 치 메뉴를 받아오는 api입니다, date format->yyyymmdd. ex)20230502")
+def get_school_cafeteria(date: str):
+    todaymenu = Dormitory(date)
     todaymenu.refine_table()
     todaymenu.get_table()
     return todaymenu.dict
 
+
 @app.get("/sentry-debug")
 async def trigger_error():
     division_by_zero = 1 / 0
-
-@app.get("/test")
-def get_dodam(date: str):
-    today=Dodam(date=date)
-    today.get_menu()
-
-    return today.menu
 
 
 
