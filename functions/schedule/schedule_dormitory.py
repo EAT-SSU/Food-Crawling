c5594737479bde757f4d2f5f595cb864c28b1159
HyunSoo <84193252+rover0811@users.noreply.github.com>
2024-04-14 11:04:57 +0000

    [Fix] replace base url to new one

--- functions/schedule/schedule_dormitory.py
+++ functions/schedule/schedule_dormitory.py
@@ -4,7 +4,7 @@ import datetime
 import requests
 from tenacity import retry,stop_after_attempt,wait_fixed
 
-from functions.common.constant import SLACK_WEBHOOK_URL
+from functions.common.constant import SLACK_WEBHOOK_URL, DORMITORY_LAMBDA_BASE_URL
 
 
 def lambda_handler(event, context):
@@ -24,8 +24,7 @@ def lambda_handler(event, context):
     }
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
 def invoke_dormitory_lambda_request(date):
-    response = requests.get("https://drvrj3q50b.execute-api.ap-northeast-2.amazonaws.com/get_dormitory",
-                       params={"date": date})
+    response = requests.get(DORMITORY_LAMBDA_BASE_URL,params={"date": date})
 
     return response
 
