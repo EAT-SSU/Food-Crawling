f1a040cebc042f35c1f3a8e61f59bbf93c25107b
rover0811 <rover0811@hotmail.com>
2025-07-15 00:17:54 +0900

    [Delete] logging은 AWS cloudwatch로 위임하여 굳이 설정 안 하기로 결정

--- functions/common/logging_config.py
+++
@@ -1,9 +0,0 @@
-import logging
-import sys
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    stream=sys.stdout,
-)
