b27e34ce9f9ab892fab9d3470b12240dac76b7d6
rover0811 <rover0811@hotmail.com>
2025-08-03 20:18:19 +0900

    [Chore] Follow PEP8 coding standards

--- functions/lambda_handlers/scheduling/haksik.py
+++ functions/lambda_handlers/scheduling/haksik.py
@@ -26,7 +26,7 @@ def haksik_schedule_view(event, context):
         container = get_container()
         scheduling_service = container.get_scheduling_service()
 
-        results:ParsedMenuData = asyncio.run(scheduling_service.process_weekly_schedule_general(
+        results: ParsedMenuData = asyncio.run(scheduling_service.process_weekly_schedule_general(
             RestaurantType.HAKSIK, weekdays, is_dev=False
         ))
 
